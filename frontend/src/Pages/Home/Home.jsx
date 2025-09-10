2d56f0f4591b75dcdb3ed9cafe12b9a95eefdbcc
harshitharavuri04 <harshitharavuri04@gmail.com>
2025-09-10 11:55:38 +0530

    changed name

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+            Harshitha url shortner
           </Title>
 
           <Text
